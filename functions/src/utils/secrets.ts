8a76266bcf406df366013982d38a5cff718949dd
take-cantik <take.cantik17@gmail.com>
2022-04-14 14:34:56 +0900

    Add: env

--- functions/src/utils/secrets.ts
+++ functions/src/utils/secrets.ts
@@ -4,3 +4,12 @@ import 'dotenv/config'
 
 export const LINE_MESSAGING_CHANNEL_ACCESS_TOKEN = process.env.LINE_MESSAGING_CHANNEL_ACCESS_TOKEN as string
 export const LINE_MESSAGING_CHANNEL_SECRET = process.env.LINE_MESSAGING_CHANNEL_SECRET as string
+
+// Firebase
+export const FIREBASE_API_KEY = process.env.FIREBASE_API_KEY as string
+export const FIREBASE_AUTH_DOMAIN = process.env.FIREBASE_AUTH_DOMAIN as string
+export const FIREBASE_PROJECT_ID = process.env.FIREBASE_PROJECT_ID as string
+export const FIREBASE_STORAGE_BUCKET = process.env.FIREBASE_STORAGE_BUCKET as string
+export const FIREBASE_MESSAGING_SENDER_ID = process.env.FIREBASE_MESSAGING_SENDER_ID as string
+export const FIREBASE_APP_ID = process.env.FIREBASE_APP_ID as string
+export const FIREBASE_MEASUREMENT_ID = process.env.FIREBASE_MEASUREMENT_ID as string
