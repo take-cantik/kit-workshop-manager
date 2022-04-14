66d21a64763e5fd2f7ff446c024116357401b7bc
take-cantik <take.cantik17@gmail.com>
2022-04-14 15:32:15 +0900

    Fix: delete firebase webapp env

--- functions/src/utils/firebase/config.ts
+++ functions/src/utils/firebase/config.ts
@@ -1,25 +1,5 @@
-// import { FirebaseOptions } from 'firebase/app'
-// import {
-//   FIREBASE_API_KEY,
-//   FIREBASE_AUTH_DOMAIN,
-//   FIREBASE_PROJECT_ID,
-//   FIREBASE_STORAGE_BUCKET,
-//   FIREBASE_MEASUREMENT_ID,
-//   FIREBASE_MESSAGING_SENDER_ID,
-//   FIREBASE_APP_ID
-// } from '~/utils/secrets'
 import admin from 'firebase-admin'
 import { FIREBASE_DATABASE_URL } from '~/utils/secrets'
-
-// export const firebaseConfig: FirebaseOptions = {
-//   apiKey: FIREBASE_API_KEY,
-//   authDomain: FIREBASE_AUTH_DOMAIN,
-//   projectId: FIREBASE_PROJECT_ID,
-//   storageBucket: FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
-//   appId: FIREBASE_APP_ID,
-//   measurementId: FIREBASE_MEASUREMENT_ID
-// }
 
 export const firebaseAdminOptions: admin.AppOptions = {
   credential: admin.credential.applicationDefault(),
