c2823c9d430a415416b7d52d1ab6ac06fdf7358e
take-cantik <take.cantik17@gmail.com>
2022-04-14 15:31:08 +0900

    Test: get from firestore

--- functions/src/triggers/https/line-bot/handlers/messages/text.ts
+++ functions/src/triggers/https/line-bot/handlers/messages/text.ts
@@ -1,10 +1,15 @@
 import { MessageEvent, TextEventMessage } from '@line/bot-sdk'
 import { lineClient, makeReplyMessage } from '~/utils/line'
 import { errorLogger } from '~/utils/util'
+import { db } from '~/utils/firebase/index'
 
 // *********
 // main関数
 // *********
+
+interface Test {
+  text: string
+}
 
 export const messageTextHandler = async (event: MessageEvent): Promise<void> => {
   try {
@@ -14,6 +19,15 @@ export const messageTextHandler = async (event: MessageEvent): Promise<void> => 
 
     if (text === 'ahi') {
       test = 'マジ卍'
+    }
+
+    const texts: Test[] = []
+    if (text === 'あひ') {
+      const res = await db.collection('tests').get()
+      res.forEach((doc) => {
+        texts.push(doc.data() as Test)
+      })
+      test = texts[0].text
     }
 
     await lineClient.replyMessage(event.replyToken, makeReplyMessage(test))
