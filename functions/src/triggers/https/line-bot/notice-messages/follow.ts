59edd5580eeacadf58ec9cf295e74fa7a42b8a61
take-cantik <take.cantik17@gmail.com>
2022-04-14 17:01:54 +0900

    Update: follow action

--- functions/src/triggers/https/line-bot/notice-messages/follow.ts
+++ functions/src/triggers/https/line-bot/notice-messages/follow.ts
@@ -1,6 +1,139 @@
-import { TextMessage } from '@line/bot-sdk'
+import { FlexMessage } from '@line/bot-sdk'
 
-export const msgFollow: TextMessage = {
-  type: 'text',
-  text: 'フォローありがとう！'
+export const msgFollow: FlexMessage = {
+  type: 'flex',
+  altText: '所属団体を教えてください',
+  contents: {
+    type: 'bubble',
+    header: {
+      type: 'box',
+      contents: [
+        {
+          type: 'text',
+          text: '友達追加ありがとうございます',
+          weight: 'bold',
+          size: 'md',
+          position: 'relative',
+          margin: 'none',
+          color: '#ffffff'
+        }
+      ],
+      layout: 'vertical',
+      paddingAll: 'xl',
+      paddingStart: 'xxl',
+      backgroundColor: '#4EB2D6'
+    },
+    body: {
+      type: 'box',
+      layout: 'vertical',
+      contents: [
+        {
+          type: 'text',
+          weight: 'bold',
+          size: 'lg',
+          text: '所属団体を教えてください',
+          wrap: true,
+          align: 'center'
+        }
+      ],
+      paddingAll: 'xxl'
+    },
+    footer: {
+      type: 'box',
+      layout: 'vertical',
+      contents: [
+        {
+          type: 'separator'
+        },
+        {
+          type: 'button',
+          action: {
+            type: 'postback',
+            label: 'プログラミング研究会',
+            data: 'プログラミング研究会',
+            displayText: 'プログラミング研究会で登録しました'
+          }
+        },
+        {
+          type: 'separator'
+        },
+        {
+          type: 'button',
+          action: {
+            type: 'postback',
+            label: 'KEPRA',
+            data: 'KEPRA',
+            displayText: 'KEPRAで登録しました'
+          }
+        },
+        {
+          type: 'separator'
+        },
+        {
+          type: 'button',
+          action: {
+            type: 'postback',
+            label: 'KITCATS',
+            data: 'KITCATS',
+            displayText: 'KITCATSで登録しました'
+          }
+        },
+        {
+          type: 'separator'
+        },
+        {
+          type: 'button',
+          action: {
+            type: 'postback',
+            label: 'DEAGLE',
+            data: 'DEAGLE',
+            displayText: 'DEAGLEで登録しました'
+          }
+        },
+        {
+          type: 'separator'
+        },
+        {
+          type: 'button',
+          action: {
+            type: 'postback',
+            label: '自然科学部',
+            data: '自然科学部',
+            displayText: '自然科学部で登録しました'
+          }
+        },
+        {
+          type: 'separator'
+        },
+        {
+          type: 'button',
+          action: {
+            type: 'postback',
+            label: '寮tech',
+            data: '寮tech',
+            displayText: '寮techで登録しました'
+          }
+        },
+        {
+          type: 'separator'
+        },
+        {
+          type: 'button',
+          action: {
+            type: 'postback',
+            label: 'その他',
+            data: 'その他',
+            displayText: 'その他で登録しました'
+          }
+        }
+      ],
+      paddingBottom: 'xs',
+      paddingTop: 'none'
+    },
+    styles: {
+      footer: {
+        separator: false
+      }
+    }
+  }
 }
