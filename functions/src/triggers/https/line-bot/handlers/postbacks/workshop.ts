790ddf37676fec86a81a5511087d07e4bdce2fa6
take-cantik <take.cantik17@gmail.com>
2022-04-15 12:10:43 +0900

    Fix: use replyToken

--- functions/src/triggers/https/line-bot/handlers/postbacks/workshop.ts
+++ functions/src/triggers/https/line-bot/handlers/postbacks/workshop.ts
@@ -1,11 +1,22 @@
 import { PostbackEvent } from '@line/bot-sdk'
+import { WorkshopFirebaseRepository } from '~/repository/WorkshopFirebaseRepository'
 import { lineClient } from '~/utils/line'
 
 export const workshopHandler = async (event: PostbackEvent): Promise<void> => {
+  const repository = new WorkshopFirebaseRepository()
+
   if (event.source.type === 'group') {
     const groupId = event.source.groupId
     const group = await lineClient.getGroupSummary(groupId)
 
-    console.log(group)
+    await repository.setWorkshop({
+      groupId,
+      groupName: group.groupName,
+      status: 'pending'
+    })
+
+    await lineClient.replyMessage(event.replyToken, { type: 'text', text: 'toursお櫛田' })
+  } else {
+    await lineClient.replyMessage(event.replyToken, { type: 'text', text: 'こんなことはありえない' })
   }
 }
