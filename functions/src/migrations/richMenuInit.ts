a5e4b53c79fc94b8784f1870994b18b6c3cf7cbb
take-cantik <take.cantik17@gmail.com>
2022-04-17 03:38:50 +0900

    Fix: change to display Text

--- functions/src/migrations/richMenuInit.ts
+++ functions/src/migrations/richMenuInit.ts
@@ -1,157 +1,152 @@
-import { Client, ClientConfig, MiddlewareConfig, RichMenu } from '@line/bot-sdk'
-import * as fs from 'fs'
-import 'dotenv/config'
+import '../alias'
+
+import { RichMenu } from '@line/bot-sdk'
+import fs from 'fs'
+import { lineClient } from '~/utils/line'
 ;(async () => {
-  const lineMiddlewareConfig: MiddlewareConfig = {
-    channelSecret: process.env.LINE_MESSAGING_CHANNEL_SECRET as string
+  try {
+    const richMenuKey: RichMenu = {
+      size: {
+        width: 2500,
+        height: 843
+      },
+      selected: false,
+      name: 'richMenuKey',
+      chatBarText: 'お知らせ',
+      areas: [
+        {
+          bounds: {
+            x: 1250,
+            y: 0,
+            width: 1250,
+            height: 200
+          },
+          action: {
+            type: 'richmenuswitch',
+            richMenuAliasId: 'richMenuInfo',
+            data: 'switchInfo'
+          }
+        },
+        {
+          bounds: {
+            x: 0,
+            y: 200,
+            width: 833,
+            height: 643
+          },
+          action: {
+            type: 'postback',
+            displayText: '借りる',
+            data: 'richMenuKey,借りる'
+          }
+        },
+        {
+          bounds: {
+            x: 833,
+            y: 200,
+            width: 834,
+            height: 643
+          },
+          action: {
+            type: 'postback',
+            displayText: '返す',
+            data: 'richMenuKey,返す'
+          }
+        },
+        {
+          bounds: {
+            x: 1667,
+            y: 200,
+            width: 833,
+            height: 643
+          },
+          action: {
+            type: 'postback',
+            displayText: '預ける',
+            data: 'richMenuKey,預ける'
+          }
+        }
+      ]
+    }
+
+    const richMenuInfo: RichMenu = {
+      size: {
+        width: 2500,
+        height: 843
+      },
+      selected: false,
+      name: 'richmenuInfo',
+      chatBarText: 'お知らせ',
+      areas: [
+        {
+          bounds: {
+            x: 0,
+            y: 0,
+            width: 1250,
+            height: 200
+          },
+          action: {
+            type: 'richmenuswitch',
+            richMenuAliasId: 'richMenuKey',
+            data: 'switchKey'
+          }
+        },
+        {
+          bounds: {
+            x: 0,
+            y: 200,
+            width: 833,
+            height: 643
+          },
+          action: {
+            type: 'postback',
+            displayText: '個人情報',
+            data: 'richMenuInfo,個人情報'
+          }
+        },
+        {
+          bounds: {
+            x: 833,
+            y: 200,
+            width: 834,
+            height: 643
+          },
+          action: {
+            type: 'postback',
+            displayText: '工房',
+            data: 'richMenuInfo,工房'
+          }
+        },
+        {
+          bounds: {
+            x: 1667,
+            y: 200,
+            width: 833,
+            height: 643
+          },
+          action: {
+            type: 'postback',
+            displayText: '使い方',
+            data: 'richMenuInfo,使い方'
+          }
+        }
+      ]
+    }
+
+    const richMenuKeyId = await lineClient.createRichMenu(richMenuKey)
+    const richMenuInfoId = await lineClient.createRichMenu(richMenuInfo)
+
+    const bufferKey = fs.readFileSync(`${__dirname}/../../assets/images/richMenuKey.jpg`)
+    const bufferInfo = fs.readFileSync(`${__dirname}/../../assets/images/richMenuInfo.jpg`)
+
+    await lineClient.setRichMenuImage(richMenuKeyId, bufferKey)
+    await lineClient.setRichMenuImage(richMenuInfoId, bufferInfo)
+
+    await lineClient.setDefaultRichMenu(richMenuKeyId)
+
+    await lineClient.createRichMenuAlias(richMenuKeyId, 'richMenuKey')
+    await lineClient.createRichMenuAlias(richMenuInfoId, 'richMenuInfo')
+  } catch (err) {
+    console.error(err)
   }
-
-  const lineConfig: ClientConfig = {
-    ...lineMiddlewareConfig,
-    channelAccessToken: process.env.LINE_MESSAGING_CHANNEL_ACCESS_TOKEN as string
-  }
-
-  const lineClient = new Client(lineConfig)
-
-  const richMenuKey: RichMenu = {
-    size: {
-      width: 2500,
-      height: 843
-    },
-    selected: false,
-    name: 'richMenuKey',
-    chatBarText: 'お知らせ',
-    areas: [
-      {
-        bounds: {
-          x: 1250,
-          y: 0,
-          width: 1250,
-          height: 200
-        },
-        action: {
-          type: 'richmenuswitch',
-          richMenuAliasId: 'richMenuInfo',
-          data: 'switchInfo'
-        }
-      },
-      {
-        bounds: {
-          x: 0,
-          y: 200,
-          width: 833,
-          height: 643
-        },
-        action: {
-          type: 'postback',
-          text: '借りる',
-          data: 'richMenuKey,借りる'
-        }
-      },
-      {
-        bounds: {
-          x: 833,
-          y: 200,
-          width: 834,
-          height: 643
-        },
-        action: {
-          type: 'postback',
-          text: '返す',
-          data: 'richMenuKey,返す'
-        }
-      },
-      {
-        bounds: {
-          x: 1667,
-          y: 200,
-          width: 833,
-          height: 643
-        },
-        action: {
-          type: 'postback',
-          text: '預ける',
-          data: 'richMenuKey,預ける'
-        }
-      }
-    ]
-  }
-
-  const richMenuInfo: RichMenu = {
-    size: {
-      width: 2500,
-      height: 843
-    },
-    selected: false,
-    name: 'richmenuInfo',
-    chatBarText: 'お知らせ',
-    areas: [
-      {
-        bounds: {
-          x: 0,
-          y: 0,
-          width: 1250,
-          height: 200
-        },
-        action: {
-          type: 'richmenuswitch',
-          richMenuAliasId: 'richMenuKey',
-          data: 'switchKey'
-        }
-      },
-      {
-        bounds: {
-          x: 0,
-          y: 200,
-          width: 833,
-          height: 643
-        },
-        action: {
-          type: 'postback',
-          text: '個人情報',
-          data: 'richMenuInfo,個人情報'
-        }
-      },
-      {
-        bounds: {
-          x: 833,
-          y: 200,
-          width: 834,
-          height: 643
-        },
-        action: {
-          type: 'postback',
-          text: '工房',
-          data: 'richMenuInfo,工房'
-        }
-      },
-      {
-        bounds: {
-          x: 1667,
-          y: 200,
-          width: 833,
-          height: 643
-        },
-        action: {
-          type: 'postback',
-          text: '使い方',
-          data: 'richMenuInfo,使い方'
-        }
-      }
-    ]
-  }
-
-  const richMenuKeyId = await lineClient.createRichMenu(richMenuKey)
-  const richMenuInfoId = await lineClient.createRichMenu(richMenuInfo)
-
-  const bufferKey = fs.readFileSync(`${__dirname}/../../assets/images/richMenuKey.jpg`)
-  const bufferInfo = fs.readFileSync(`${__dirname}/../../assets/images/richMenuInfo.jpg`)
-
-  await lineClient.setRichMenuImage(richMenuKeyId, bufferKey)
-  await lineClient.setRichMenuImage(richMenuInfoId, bufferInfo)
-
-  await lineClient.setDefaultRichMenu(richMenuKeyId)
-
-  await lineClient.createRichMenuAlias(richMenuKeyId, 'richMenuKey')
-  await lineClient.createRichMenuAlias(richMenuInfoId, 'richMenuInfo')
 })()
