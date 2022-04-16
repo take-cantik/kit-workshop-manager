7f7ccf2b8d842139acddfb43c286581409148eaf
take-cantik <take.cantik17@gmail.com>
2022-04-16 17:41:56 +0900

    Update: use uuid

--- functions/src/triggers/https/line-bot/handlers/postbacks/index.ts
+++ functions/src/triggers/https/line-bot/handlers/postbacks/index.ts
@@ -1,5 +1,7 @@
 import { PostbackEvent } from '@line/bot-sdk'
-import { getPrefix } from '~/utils/postback'
+import { UuidFirebaseRepository } from '~/repository/UuidFirebaseRepository'
+import { lineClient } from '~/utils/line'
+import { getPrefix, getUuid } from '~/utils/postback'
 import { groupHandler } from './group'
 import { pendingHandler } from './pending'
 import { workshopChangeHandler } from './workshops/workshopChange'
@@ -7,27 +9,34 @@ import { workshopInitHandler } from './workshops/workshopInit'
 import { v4 as uuidv4 } from 'uuid'
 
 export const postbackHandler = async (event: PostbackEvent): Promise<void> => {
-  const uuid = uuidv4()
+  const uuidRepository = new UuidFirebaseRepository()
 
-  const prefix = getPrefix(event)
+  const eventUuid = getUuid(event)
+  if (await uuidRepository.existUuid(eventUuid)) {
+    await lineClient.replyMessage(event.replyToken, { type: 'text', text: 'このメッセージは既に使用されています' })
+  } else {
+    const prefix = getPrefix(event)
+    const uuid = uuidv4()
+    await uuidRepository.addUuid(uuid)
 
-  if (prefix === 'group') {
-    await groupHandler(event)
-  }
+    if (prefix === 'group') {
+      await groupHandler(event)
+    }
 
-  if (prefix === 'workshopInit') {
-    await workshopInitHandler(event)
-  }
+    if (prefix === 'workshopInit') {
+      await workshopInitHandler(event)
+    }
 
-  if (prefix === 'workshopChange') {
-    await workshopChangeHandler(event)
-  }
+    if (prefix === 'workshopChange') {
+      await workshopChangeHandler(event)
+    }
 
-  if (prefix === 'pending') {
-    await pendingHandler(event)
-  }
+    if (prefix === 'pending') {
+      await pendingHandler(event)
+    }
 
-  if (prefix === 'userChange') {
-    //
+    if (prefix === 'userChange') {
+      //
+    }
   }
 }
