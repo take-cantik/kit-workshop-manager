d2d9772d5d0b8e3e63d8f152d1c59bd38ed182f9
take-cantik <take.cantik17@gmail.com>
2022-08-06 11:49:05 +0900

    Commentout pubsub function

--- functions/src/triggers/pubsub/line-bot/index.ts
+++ functions/src/triggers/pubsub/line-bot/index.ts
@@ -1,30 +1,30 @@
-import { middleware } from '@line/bot-sdk'
-import { logger, region, RuntimeOptions } from 'firebase-functions/v1'
-import { lineMiddlewareConfig } from '~/utils/line'
-import { errorLogger } from '~/utils/util'
-import { handler } from './handlers'
+// import { middleware } from '@line/bot-sdk'
+// import { logger, region, RuntimeOptions } from 'firebase-functions/v1'
+// import { lineMiddlewareConfig } from '~/utils/line'
+// import { errorLogger } from '~/utils/util'
+// import { handler } from './handlers'
 
-middleware(lineMiddlewareConfig)
-const app = () => {
-  new Promise(handler)
-    .then((result) => {
-      logger.info(result)
-    })
-    .catch((err) => {
-      errorLogger(err)
-    })
-}
+// middleware(lineMiddlewareConfig)
+// const app = () => {
+//   new Promise(handler)
+//     .then((result) => {
+//       logger.info(result)
+//     })
+//     .catch((err) => {
+//       errorLogger(err)
+//     })
+// }
 
-// *************
-// Functions設定
+// // *************
+// // Functions設定
 
-const runtimeOpts: RuntimeOptions = {
-  timeoutSeconds: 540,
-  memory: '1GB'
-}
+// const runtimeOpts: RuntimeOptions = {
+//   timeoutSeconds: 540,
+//   memory: '1GB'
+// }
 
-module.exports = region('asia-northeast1')
-  .runWith(runtimeOpts)
-  .pubsub.schedule('0 21 * * *')
-  .timeZone('Asia/Tokyo')
-  .onRun(app)
+// module.exports = region('asia-northeast1')
+//   .runWith(runtimeOpts)
+//   .pubsub.schedule('0 21 * * *')
+//   .timeZone('Asia/Tokyo')
+//   .onRun(app)
