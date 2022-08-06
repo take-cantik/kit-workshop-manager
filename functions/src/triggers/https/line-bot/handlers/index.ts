8a149a280664e15771374f542ea9b092aba5483a
take-cantik <take.cantik17@gmail.com>
2022-08-06 11:49:27 +0900

    Commentout some event handlers

--- functions/src/triggers/https/line-bot/handlers/index.ts
+++ functions/src/triggers/https/line-bot/handlers/index.ts
@@ -1,25 +1,25 @@
 import { WebhookEvent } from '@line/bot-sdk'
 import { lineClient } from '~/utils/line'
 import { msgError } from '~line/notice-messages/other'
-import { followHandler } from './follow'
-import { joinHandler } from './join'
+// import { followHandler } from './follow'
+// import { joinHandler } from './join'
 import { errorLogger } from '~/utils/util'
 import { postbackHandler } from './postbacks'
 import { unfollowHandler } from './unfollow'
-import { memberJoinedHandler } from './memberJoined'
+// import { memberJoinedHandler } from './memberJoined'
 import { leaveHandler } from './leave'
 
 export const handlers = async (event: WebhookEvent): Promise<void> => {
   try {
     switch (event.type) {
-      case 'follow':
-        return await followHandler(event)
+      // case 'follow':
+      //   return await followHandler(event)
       case 'unfollow':
         return await unfollowHandler(event)
-      case 'join':
-        return await joinHandler(event)
-      case 'memberJoined':
-        return await memberJoinedHandler(event)
+      // case 'join':
+      //   return await joinHandler(event)
+      // case 'memberJoined':
+      //   return await memberJoinedHandler(event)
       case 'postback':
         return await postbackHandler(event)
       case 'leave':
